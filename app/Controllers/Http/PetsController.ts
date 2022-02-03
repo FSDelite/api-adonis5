77452b041664fa740ea4ca07ff1130a81ca38753
Felipe Delite <felipedelite@gmail.com>
2022-02-03 16:37:24 -0400

    crud done

--- app/Controllers/Http/PetsController.ts
+++ app/Controllers/Http/PetsController.ts
@@ -1,17 +1,31 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Pet from 'App/Models/Pet'
 
 export default class PetsController {
-  async index(ctx: HttpContextContract) {
-    return 'GET Pets'
+  public async index() {
+    return await Pet.all()
   }
 
-  async store({ request }: HttpContextContract) {
-    return 'POST Pets'
+  public async store({ request, response }: HttpContextContract) {
+    const data = request.all()
+    response.status(201)
+    return await Pet.create(data)
   }
 
-  async show({ params }: HttpContextContract) {}
+  public async show({ params }: HttpContextContract) {
+    return await Pet.findOrFail(params.id)
+  }
 
-  async update({ params }: HttpContextContract) {}
+  public async update({ params, request }: HttpContextContract) {
+    const data = request.all()
+    const pet = await Pet.findOrFail(params.id)
+    pet.merge(data)
+    return await pet.save()
+  }
 
-  async destroy({ params }: HttpContextContract) {}
+  public async destroy({ params, response }: HttpContextContract) {
+    const pet = await Pet.findOrFail(params.id)
+    response.status(204)
+    return pet.delete()
+  }
 }
