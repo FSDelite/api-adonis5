4323e97d7a29b494cfbedc4a4fa7213f177b41e7
Felipe Delite <felipedelite@gmail.com>
2022-02-03 16:15:36 -0400

    principais funçoes do crud para pets

--- app/Controllers/Http/PetsController.ts
+++ app/Controllers/Http/PetsController.ts
@@ -1,3 +1,17 @@
-// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
-export default class PetsController {}
+export default class PetsController {
+  async index(ctx: HttpContextContract) {
+    return 'GET Pets'
+  }
+
+  async store({ request }: HttpContextContract) {
+    return 'POST Pets'
+  }
+
+  async show({ params }: HttpContextContract) {}
+
+  async update({ params }: HttpContextContract) {}
+
+  async destroy({ params }: HttpContextContract) {}
+}
